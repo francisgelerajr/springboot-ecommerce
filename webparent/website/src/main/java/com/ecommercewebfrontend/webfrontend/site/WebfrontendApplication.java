2584ce7bcee85ff2ba34e3ad8cd9f784282fa343
kurokoyy <francisgelerajr@gmail.com>
2023-08-03 20:26:52 +0800

    ecommerce server initial commit

---
+++ webparent/website/src/main/java/com/ecommercewebfrontend/webfrontend/site/WebfrontendApplication.java
@@ -0,0 +1,13 @@
+package com.ecommercewebfrontend.webfrontend.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfrontendApplication.class, args);
+	}
+
+}
---
+++ webparent/website/src/test/java/com/ecommercewebfrontend/webfrontend/site/WebfrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommercewebfrontend.webfrontend.site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
